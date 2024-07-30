713f467c356dd7ac882987df77a6183ec313aa53
Carlos Rodrigues <cadu.mrod@gmail.com>
2024-07-30 16:07:28 -0300

    Adicionadas funcoes de status de tarefa e remocao de tarefa

--- bd.py
+++ bd.py
@@ -2,6 +2,8 @@ import sqlite3
 # Script que cria e/ou manipula o banco de dados
 
 
+################################# FUNÇÕES BD #########################################
+
 # Função para conectar no banco de dados
 def conectar_banco_dados(tarefas_db):
     conexao = sqlite3.connect(tarefas_db)
@@ -29,6 +31,8 @@ conexao = conectar_banco_dados('tarefas.db')
 criar_tabela(conexao)
 
 
+################################# FUNÇÕES APP #########################################
+
 # Função para adicionar tarefas
 def adicionar_tarefas(conexao, tarefa):
     cursor = conexao.cursor()
@@ -44,8 +48,23 @@ def visualizar_tarefas(conexao):
     return cursor.fetchall()
 
 
-adicionar_tarefas(conexao, 'Limpar sala')
-adicionar_tarefas(conexao, 'Limpar cozinha')
+# Função para alterar status de tarefa
+def alterar_status(conexao, id, novo_status):
+    cursor = conexao.cursor()
+    cursor.execute("UPDATE Tarefas SET status = ? WHERE id = ?",
+                   (novo_status, id))
+    conexao.commit()
+
+
+# Função para remover tarefas
+def excluir_tarefa(conexao, id):
+    cursor = conexao.cursor()
+    cursor.execute("DELETE FROM Tarefas WHERE id = ?", (id,))
+    conexao.commit()
+
+
+excluir_tarefa(conexao, 1)
+excluir_tarefa(conexao, 2)
 print(visualizar_tarefas(conexao))
 
 # Encerrar conexão
